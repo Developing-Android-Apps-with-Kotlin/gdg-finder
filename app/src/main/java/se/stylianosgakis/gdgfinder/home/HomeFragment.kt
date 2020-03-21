d2a0cbb968d9b94a4ec5ed1d1479ba6cc31ca337
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-21 12:23:28 +0100

    Add floating button that navigates to the GdgList fragment on click

--- app/src/main/java/se/stylianosgakis/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/se/stylianosgakis/gdgfinder/home/HomeFragment.kt
@@ -6,10 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import se.stylianosgakis.gdgfinder.R
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import se.stylianosgakis.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
-
     companion object {
         fun newInstance() = HomeFragment()
     }
@@ -20,7 +21,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
-        return view
+        val binding = HomeFragmentBinding.inflate(inflater)
+        binding.apply {
+            viewModel = this@HomeFragment.viewModel
+        }
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { doNavigate ->
+            if (doNavigate) {
+                findNavController().navigate(
+                    HomeFragmentDirections.actionHomeFragmentToGdgListFragment()
+                )
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
