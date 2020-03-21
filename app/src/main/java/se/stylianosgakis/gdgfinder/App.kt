2961c904c5dece97a5913c8a7554c28a11fb9318
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-21 21:26:01 +0100

    Add colors for dark mode and enable when battery save mode is set

---
+++ app/src/main/java/se/stylianosgakis/gdgfinder/App.kt
@@ -0,0 +1,11 @@
+package se.stylianosgakis.gdgfinder
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+    }
+}
